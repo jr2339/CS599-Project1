2bb3c3e58f893c6a856cbfecb2a68d8de2351089
jun <jr2339@nau.edu>
2016-09-16 15:58:16 -0700

    Add some comments

--- ppm.c
+++ ppm.c
@@ -10,25 +10,18 @@
 #include <ctype.h>
 #include <string.h>
 #include "ppm.h"
-
 int readMagicNumber(FILE *fp) {
-   
     int magic_number;
     if (!fscanf(fp, "P%d\n", &magic_number)){
         fprintf(stderr,"file is not in PPM format, we can't read it");
         exit(1);
     }
-    
-    //magic_number = ch - '0';
     return magic_number;
 }
-
 /**************************************************************************************************************
  **************************************************************************************************************/
-// readPPMHeader help us to detemine which format(such as P1, P2...) for our source image
-
+// readPPMHeader help us to detemine which format is our source image
 Image *readPPMHeader(FILE *f_source, Image *buffer){
-    //Image *buffer = (Image *)malloc(sizeof(Image));
     char ch;
     /*skip the comments*/
     ch = getc(f_source); // ch is int
@@ -49,12 +42,12 @@ Image *readPPMHeader(FILE *f_source, Image *buffer){
     else{
         ungetc(ch, f_source); //put that digital back
     }
+    /*default tupletype and depth for P3 is RGB and 3*/
     buffer->tupltype = "RGB";
     buffer->depth = 3;
     //read the width, height,amd maximum value for a pixel
     fscanf(f_source, "%d %d %d\n",&buffer->width,&buffer->height,&buffer->maxval);
-    
-    buffer->data = (u_char *)malloc(buffer->width*buffer->height*3);// G, R, B three colors
+    buffer->data = (u_char *)malloc(buffer->width*buffer->height*4);// G, R, B three colors
     if(buffer->maxval >= 65336 ||buffer->maxval <= 0){
         fprintf(stderr,"image is not ture-color(8byte), read failed\n");
         exit(1);
@@ -62,7 +55,6 @@ Image *readPPMHeader(FILE *f_source, Image *buffer){
     return buffer;
     
 }
-
 /**************************************************************************************************************
  **************************************************************************************************************/
 Image *readPAMHeadr(FILE *f_source, Image *buffer){
@@ -71,30 +63,23 @@ Image *readPAMHeadr(FILE *f_source, Image *buffer){
     char array3[1024];
     while(1){
         fscanf(f_source, "%s", array1);
-        if(strcmp(array1, "ENDHDR") == 0){
-            buffer->data = (u_char *)malloc(buffer->width*buffer->height*3);// G, R, B three colors
-            printf("buffer data created successful\n");
+        if(strcmp(array1, "ENDHDR") == 0){   // we read the whole header
+            buffer->data = (u_char *)malloc(buffer->width*buffer->height*4);// G, R, B three colors
             return buffer;
-            
-        }else if(strcmp(array1, "TUPLTYPE") == 0){
+        }else if(strcmp(array1, "TUPLTYPE") == 0){ // if it is tupltype, we put value to it
             fscanf(f_source, "%s", array3);
             buffer->tupltype = array3;
-            printf("the tupltype is %s \n", array3);
         }
         else{
-            fscanf(f_source, "%d", &array2);
+            fscanf(f_source, "%d", &array2);      // read width, height and depth , maxvalue at here
             if(strcmp(array1, "WIDTH") == 0){
                 buffer->width = array2;
-                printf("buffer width is : %d \n", array2);
             }else if(strcmp(array1, "HEIGHT") == 0){
                 buffer->height = array2;
-                printf("buffer height is : %d\n", array2);
             }else if(strcmp(array1, "DEPTH") == 0){
                 buffer->depth = array2;
-                printf("buffer depth is : %d\n", array2);
             }else if(strcmp(array1, "MAXVAL") == 0){
                 buffer->maxval = array2;
-                printf("buffer maxval is : %d\n", array2);
             }
         }
     }
@@ -103,12 +88,10 @@ Image *readPAMHeadr(FILE *f_source, Image *buffer){
 /**************************************************************************************************************
  **************************************************************************************************************/
 Image *ImageRead(const char *filename){
-    printf("9999998888\n");
     FILE *fp = fopen(filename,"r");
     if(!fp){
         fprintf(stderr,"can't open file for reading \n");
         exit(1);
-        
     }
     Image *buffer = (Image *)malloc(sizeof(Image));
     if(!buffer){
@@ -117,7 +100,6 @@ Image *ImageRead(const char *filename){
     }
     buffer->magic_number = readMagicNumber(fp);
     buffer->data = (u_char *)malloc(buffer->width*buffer->height*4);// G, R, B three colors and A
-    
     if(buffer->magic_number==3){
         readPPMHeader(fp,buffer);
         buffer->data = (u_char *)malloc(buffer->width*buffer->height*4);
@@ -183,11 +165,11 @@ Image *ImageRead(const char *filename){
             }else if (strcmp(buffer->tupltype, "RGB")==0){
                 int size3 = buffer->height*buffer->width*3;
                 char c='A';//init
-                int k=0;
+                int k=0;    // each time k incrsement 3
                 for(int m=0;m<size3;m+=3){
-                    fread(&c, 1, 1, fp);
-                    buffer->data[k] = c;
-                    k += 1;
+                    fread(&c, 1, 1, fp);   // in tis 3 times we put RGB
+                    buffer->data[k] = c;   // into the buffer, and let the
+                    k += 1;                //4th one eaual 255
                     fread(&c, 1, 1, fp);
                     buffer->data[k] = c;
                     k += 1;
@@ -197,8 +179,6 @@ Image *ImageRead(const char *filename){
                     buffer->data[k] = 255;
                     k += 1;
                 }
-                printf("the k is %d\n",k);
-                printf("the size 2 is %d\n", size2);
             if (k != size2) {
             fprintf(stderr,"cannot read image datat from file \n");
             exit(1);
@@ -207,24 +187,21 @@ Image *ImageRead(const char *filename){
     }
     return buffer;
 }
-
 /**************************************************************************************************************
  **************************************************************************************************************/
-
 void ImageWrite(Image *buffer, const char *filename,int format){
     int num2;
     int size = buffer->width * buffer->height * 4;
-    FILE *f_des = fopen(filename, "w"); //filename looks like /home/jun
+    FILE *f_des = fopen(filename, "w");
     if (!f_des){
         fprintf(stderr,"cannot open file for writing");
-        
     }
     if(format==6){
         fprintf(f_des, "P%d\n%d %d\n%d\n",format,buffer->width, buffer->height, buffer->maxval);
-        for(int i=1; i<size+1;i++){
+        for(int i=1; i<size+1;i++){    // for each for slots we skip it,
             char ch=buffer->data[i-1];
             if (i%4 !=0) {
-               fwrite(&ch, 1, 1, f_des);
+               fwrite(&ch, 1, 1, f_des);  // which means we skip A at here
             }
         }
     }
@@ -233,29 +210,26 @@ void ImageWrite(Image *buffer, const char *filename,int format){
         int i,j;
         for(i=0;i<buffer->height;i++){
             for(j=0;j<buffer->width;j++){
-            
-                fprintf(f_des, "%d\n",buffer->data[i*buffer->width*4+4*j]);
-                fprintf(f_des, "%d\n",buffer->data[i*buffer->width*4+4*j+1]);
-                fprintf(f_des, "%d\n",buffer->data[i*buffer->width*4+4*j+2]);
+                fprintf(f_des, "%d\n",buffer->data[i*buffer->width*4+4*j]); // We got R and put in to the Buffer
+                fprintf(f_des, "%d\n",buffer->data[i*buffer->width*4+4*j+1]);// We got G and put in to the Buffer
+                fprintf(f_des, "%d\n",buffer->data[i*buffer->width*4+4*j+2]);// We got B and put in to the Buffer
                 //Skip the A
             }
-                
             fprintf(f_des, "\n");
         }
     }
     else if(format==7){
         fprintf(f_des,"P%d\nWIDTH %d\nHEIGHT %d\nDEPTH %d\nMAXVAL %d\nTUPLTYPE %s\nENDHDR\n",format,buffer->width,buffer->height,buffer->depth,buffer->maxval,buffer->tupltype);
         if (strcmp(buffer->tupltype, "RGB_ALPHA") == 0) {
-            num2 = fwrite((void *) buffer->data, 1, (size_t) size, f_des);
+            num2 = fwrite((void *) buffer->data, 1, (size_t) size, f_des);// We use fwrite
         }else if (strcmp(buffer->tupltype, "RGB")==0){
-            for(int i=1; i<size+1;i++){
+            for(int i=1; i<size+1;i++){   // for each four slots we skip it
                 char ch=buffer->data[i-1];
                 if (i%4 !=0) {
                     fwrite(&ch, 1, 1, f_des);
                 }
             }
         }
-        
     fclose(f_des);
     }
 }
